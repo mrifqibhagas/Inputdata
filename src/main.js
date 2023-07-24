63fd8103e27316cd5978ee0b2675ba55cc11f28c
mrifqibhagas <75106809+mrifqibhagas@users.noreply.github.com>
2023-07-24 17:49:58 +0700

    main.js

--- src/main.js
+++ src/main.js
@@ -1,53 +1,55 @@
-//import { connectToNetwork, connectToRouter } from "@ndn/autoconfig";
-import { Endpoint } from "@ndn/endpoint";
 import { AltUri, Interest, Name } from "@ndn/packet";
 import { WsTransport } from "@ndn/ws-transport";
-//import { delay } from "@ndn/util";   
+import { Endpoint } from "@ndn/endpoint";
 
- // Fungsi untuk menyimpan data ke Firestore
- try{ 
-    function simpanData(evt) {
-         const nama = document.getElementById('nama').value;
-         const umur = document.getElementById('umur').value;
-         const sex = document.querySelector('input[name="sex"]:checked').value;
-         const prefix = new Name(document.querySelector('prefix').value);
-         const penyakit = document.getElementById('penyakit').value;
-        // $log.textContent = `Check Data \n${AltUri.ofName(prefix)}\n`;
+document.getElementById('data_pasien_form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission
+    const nama = document.getElementById('nama').value;
+    const umur = document.getElementById('umur').value;
+    const sex = document.getElementById('jenis_kelamin').value;
+    const penyakit = document.getElementById('penyakit').value;
 
-        const dataJson = {
-            nama: nama,
-            umur: umur,
-            sex: sex,
-            // interest: interest,
-            penyakit: penyakit
-        };
-         const jsonData = JSON.stringify(dataJson);
-         const endpoint = new Endpoint();
-         const encoder = new TextEncoder();
-         const app = jsonData;}
+    // Create a JSON object from the input values
+    const dataJson = {
+        nama: nama,
+        umur: umur,
+        sex: sex,
+        penyakit: penyakit
+    };
 
-         for (let i = 0; i < 1; ++i) {
-            const interest = new Interest();
-            interest.name = prefix;
-            interest.mustBeFresh = true; 
-            interest.lifetime = 10000;
-            interest.appParameters = encoder.encode(app);
-            //$log.textContent += `\n${encoder.encode(app)}\n`;
-            const t0 = Date.now();
-            await interest.updateParamsDigest();
-            try {
-                // Retrieve Data and compute round-trip time.
-                const data = await endpoint.consume(interest);
-                const rtt = Date.now() - t0;
-                const dataContent = data.content;}
-            finally {
-                console.log("Berhasil mengirim");}
-        }
+    const jsonData = JSON.stringify(dataJson);
+
+    async function ping(evt) { //membuat fungsi async
+        evt.preventDefault();
+        const prefix = "/data/inputdata"; //membuat const baru dari id #app_prefix dari form
+        const app = jsonData; //membuat const baru dari id #app_param dari form
+        //const $log = document.querySelector("#app_log"); //membuat const baru dari id #app_log dari form
+        
+        const endpoint = new Endpoint();
+        const encoder = new TextEncoder(); //membuat const baru untuk fungsi TextEncoder
+        const interest = new Interest(); //membuat const baru untuk fungsi Interest
+
+        interest.name = prefix; //membuat const baru untuk dari fungsi interest dan name
+        interest.mustBeFresh = true; 
+        interest.lifetime = 1000;
+        interest.appParameters = encoder.encode(app); //melakukan encode packet ndn
+        await interest.updateParamsDigest();    
+
+
+        const t0 = Date.now();
+        const data = await endpoint.consume(interest);
+        const rtt = Date.now() - t0;
+        const dataContent = data.content;
+
+        $log.textContent += `content= ${String.fromCharCode(...dataContent)}\n`; //print data respon
+        console.log(interest.appParameters)
+        console.log(`${rtt} ms`);
     }
-finally {
-    console.log("Berhasil menambah data");
-}
+});
+
 async function main() {
-    const face = await WsTransport.createFace({}, "wss://scbe.ndntel-u.my.id:9696");
-    face.addRoute(new Name("/"));}
-    window.addEventListener("load", main);
+    const face = await WsTransport.createFace({}, "wss://hmbe.ndntel-u.my.id:9696");
+    face.addRoute(new Name("/"));
+    document.querySelector("#app_form").addEventListener("submit", ping);
+  }
+  window.addEventListener("load", main);
